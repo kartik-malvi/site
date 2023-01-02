cfb1d07b4665b305389dd5b7a48c890d0b78e007
kartik-malvi <kartikmalviya.oo9@gmail.com>
2023-01-02 18:33:42 +0530

    about section is work on progress

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const Social = () =>{
+    return(
+        <div className="home__social">
+            <a href="https://www.instagram.com/malvi_kartik09/" className="home__social-icon" target="_blank" rel="noopener">
+            <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/kartik-malviya-3a9a821b2/" className="home__social-icon" target="_blank" rel="noopener">
+            <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/kartik-malvi" className="home__social-icon" target="_blank" rel="noopener">
+            <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+export default Social
