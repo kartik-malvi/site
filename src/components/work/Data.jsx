0a5d7f2326805b5a3ecef5da01089dcd2b02710a
kartik-malvi <kartikmalviya.oo9@gmail.com>
2023-01-12 17:30:37 +0530

    adding links in work

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -10,24 +10,29 @@ export const projectsData = [
         image:Work1,
         title:'Vim & Vigour',
         category:'Development',
+        link:'https://kartik-malvi.github.io/vim-vigour/',
+       
     },
     {
         id:2,
         image:Work2,
         title:'Last resort recovery',
         category:'Wordpress',
+        link:'https://staging-lastresortrecovery-staging.kinsta.cloud/',
     },
     {
         id:3,
         image:Work3,
         title:'Johnson stone work',
         category:'Wordpress',
+        link:'https://www.johnsonstonework.com/',
     },
     {
         id:4,
         image:Work4,
         title:'Access Vascular Health',
         category:'Wordpress',
+        link:'https://accessvascularhealth.com/',
     },
    
     {
