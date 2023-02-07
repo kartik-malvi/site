cc6307a0db269b95abe719ca687c5d04e7545ed7
kartik-malvi <kartikmalviya.oo9@gmail.com>
2023-02-07 12:07:45 +0530

    adding william mills

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,6 +3,7 @@ import Work2 from "../../assets/work2.png"
 import Work3 from "../../assets/work3.png"
 import Work4 from "../../assets/work4.png"
 import Work5 from "../../assets/work5.png"
+import Work6 from "../../assets/work6.png"
 export const projectsData = [
 
     {
@@ -42,6 +43,13 @@ export const projectsData = [
         category:'Development',
 
     },
+    {
+        id:6,
+        image:Work6,
+        title:'William Mills',
+        category:'Wordpress',
+        link:"https://williammills.uk/it-it/",
+    }
 ];
 
 export const projectsNav = [
--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -6,7 +6,7 @@ const WorksItems = ({item}) => {
         <img src={item.image} alt="" className="work__img"  />
         <h3 className="work__title">{item.title}</h3>
         <a href={item.link} className="work__button ">
-            Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            Visit <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
      </div>
     )
